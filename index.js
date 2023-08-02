27620710ad6e43550d92109755cf507f76257bfd
IAFEnvoy <83523430+IAFEnvoy@users.noreply.github.com>
2023-08-02 14:03:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const gen=()=>{
   let base64=window.btoa(url);
   document.getElementById("ans").innerText=`转码后的URL为：https://link.iafenvoy.net/?${base64}`;
 }
+
+window.onload=()=>{
+  let base64=location.search;
+  if(base64==null||base64=="") return;
+  base64=base64.substring(1);
+  let url=window.atob(base64);
+  location.href=url;
+}
